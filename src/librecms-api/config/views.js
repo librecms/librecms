ce4e2e653819c97338114819b5b464585a1750f6
zdwolfe <wolfe.zach@gmail.com>
2013-09-11 19:41:26 -0400

    Initial Setup of barebones RESTful API With example User model.
    Squashed commit of the following:
    
    commit aae163f8fce44d723339626a7e7f8598475cc703
    Author: zdwolfe <wolfe.zach@gmail.com>
    Date:   Wed Sep 11 19:39:23 2013 -0400
    
        Add User model and controller for example purpose.
    
    commit e8f7c419839d09943962043724bf0611bfd448e3
    Author: zdwolfe <wolfe.zach@gmail.com>
    Date:   Wed Sep 11 19:38:23 2013 -0400
    
        Add Waterline adapter for MongoDB
    
    commit 631e6d91e401bbc8093b8ee3e7585e71ae52513e
    Author: zdwolfe <wolfe.zach@gmail.com>
    Date:   Wed Sep 11 19:20:34 2013 -0400
    
        remove passport as dependency
    
    commit b40b6ff001ddf1729ad6fe754e8da9f6b3307862
    Author: zdwolfe <wolfe.zach@gmail.com>
    Date:   Wed Sep 11 19:18:40 2013 -0400
    
        Removed views, views configuration, public assets, as these will be handled by client

--- src/librecms-api/config/views.js
+++ src/librecms-api/config/views.js
@@ -10,60 +10,6 @@
  */
 
 module.exports.views = {
-
-  // View engine (aka template language)
-  // to use for your app's *server-side* views
-  // 
-  // Sails+Express supports all view engines which implement
-  // TJ Holowaychuk's `consolidate.js`, including, but not limited to:
-  // 
-  // ejs, jade, handlebars, mustache
-  // underscore, hogan, haml, haml-coffee, dust
-  // atpl, eco, ect, jazz, jqtpl, JUST, liquor, QEJS, 
-  // swig, templayed, toffee, walrus, & whiskers
-
   engine: 'ejs',
-
-
-
-  // Layouts are simply top-level HTML templates you can use as wrappers 
-  // for your server-side views.  If you're using ejs or jade, you can take advantage of
-  // Sails' built-in `layout` support.
-  // 
-  // When using a layout, when one of your views is served, it is injected into
-  // the `body` partial defined in the layout.  This lets you reuse header
-  // and footer logic between views.
-  //
-  // NOTE:  Layout support is only implemented for the `ejs` view engine!
-  //        For most other engines, it is not necessary, since they implement 
-  //        partials/layouts themselves.  In those cases, this config will be silently 
-  //        ignored.
-  //
-  // The `layout` setting may be set to one of:
-  // 
-  // If `true`, Sails will look for the default, located at `views/layout.ejs`
-  // If `false`, layouts will be disabled.
-  // Otherwise, if a string is specified, it will be interpreted as the relative path 
-  // to your layout from `views/` folder.
-  // (the file extension, e.g. ".ejs", should be omitted)
-  //
-
-  layout: 'layout'
-
-
-
-  // Using Multiple Layouts with EJS
-  //
-  // If you're using the default engine, `ejs`, Sails supports the use of multiple
-  // `layout` files.  To take advantage of this, before rendering a view, override
-  // the `layout` local in your controller by setting `res.locals.layout`.
-  // (this is handy if you parts of your app's UI look completely different from each other)
-  //
-  // e.g. your default might be
-  // layout: 'layouts/public'
-  // 
-  // But you might override that in some of your controllers with:
-  // layout: 'layouts/internal'
-
-
+  layout: 'false'
 };
