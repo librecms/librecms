9c84ec263af0c2250ceb95e622130bf178aedc56
zdwolfe <wolfe.zach@gmail.com>
2013-09-12 11:21:45 -0400

    Initial setup and scaffolding of angular app.

---
+++ src/angular/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('librecmsApp', [])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  });
