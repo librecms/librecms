c64ca6986d5d081bfd9bae125b812cd9c5711a69
zdwolfe <wolfe.zach@gmail.com>
2013-10-11 16:25:50 -0400

    Fix style from navbar

--- src/angular/app/scripts/controllers/navbar.js
+++ src/angular/app/scripts/controllers/navbar.js
@@ -3,23 +3,27 @@
 angular.module('librecmsApp')
   .controller('NavbarCtrl', function ($scope) {
     console.log('Hello from NavbarCtrl');
-    $scope.courses = [
+    var courses = [
       { 
-        name: 'CMPSC483w',
-        id: '483'
+        name: "CMPSC483w",
+        id: "483"
       },
       {
-        name: 'MATH485',
-        id: '485'
+        name: "MATH485",
+        id: "485"
       },
       {
-        name: 'MATH230',
-        id: '230'
+        name: "MATH230",
+        id: "230"
       },
       {
-        name: 'CMPEN362',
+        name: "CMPEN362",
         id: 362
       }
-    ]
-    $scope.currentCourse = $scope.courses[0];
+    ];
+
+    $scope.user = {
+      fullname: "Zachary Wolfe",
+      courses: courses
+    }
   });
