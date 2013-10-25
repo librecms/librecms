4197a13804992d35e4fd1dca2fc58cb02c08674d
zdwolfe <wolfe.zach@gmail.com>
2013-10-25 17:36:53 -0400

    Add faux data to assignment. Move from ContentCtrl to AssignmentCtrl

--- src/angular/app/scripts/controllers/Assignment.js
+++ src/angular/app/scripts/controllers/Assignment.js
@@ -4,5 +4,12 @@ angular.module('librecmsApp')
   .controller('AssignmentCtrl', function ($scope, $stateParams) {
     $scope.assignmentId = $stateParams.assignmentId;
     $scope.assignment = {
+      name: 'Homework 5',
+      description: 'Do questions 1, 2, 3 and 4 from chapter 5',
+      attachments: [
+        {
+          title: 'SOMEPDF.pdf'
+        }
+      ]
     };
   });
