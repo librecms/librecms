4743faefdf0a61d9ce9d7869b799fe56c5e87d1b
Mike <mjc0042@gmail.com>
2013-11-15 16:41:47 -0500

    Averages for grades tab

--- src/angular/app/scripts/controllers/grades.js
+++ src/angular/app/scripts/controllers/grades.js
@@ -6,42 +6,42 @@ angular.module('librecmsApp')
   var attendance = [
     {
       title: 'oct1',
-      score: '100%'
+      score: '100'
     },
     {
       title: 'oct2',
-      score: '100%'
+      score: '100'
     }
   ];
   var homework = [
     {
       title: 'hw1',
-      score: '90%',
-      average: '42%'
+      score: '90',
+      average: '42'
     },
     {
       title: 'gEt SoMe',
-      score: '3%',
-      average: '0%'
+      score: '3',
+      average: '0'
     }
   ];
   var exams = [
     {
       title: 'shit show number 1',
-      score: '20%',
-      average: '25%'
+      score: '20',
+      average: '25'
     },
     { 
       title: 'shit show number 2: Your still here?',
-      score: '18%',
-      average: '19%'
+      score: '18',
+      average: '19'
     }
   ];
   var quizzes = [
     {
       title: 'waste of time but required to give this',
-      score: '85%',
-      average: '60%'
+      score: '85',
+      average: '60'
     }
   ];
   var other = [];
@@ -51,6 +51,27 @@ angular.module('librecmsApp')
   all.push.apply(all,homework);
   all.push.apply(all,quizzes);
   all.push.apply(all,other);
+
+  // Calculate Homework Average
+  var hwAvg = 0;
+  for (var i = 0; i < homework.length; i++) {
+    hwAvg = (hwAvg + parseInt(homework[i].score)) / (i+1);
+  }
+  $scope.homeworkAverage = hwAvg + "%";
+
+  // Calculate Quiz Average
+  var quizAvg = 0;
+  for (var i = 0; i < quizzes.length; i++) {
+    quizAvg = (quizAvg + parseInt(quizzes[i].score)) / (i+1);
+  }
+  $scope.quizAverage = quizAvg + "%";
+
+  // Calculate Quiz Average
+  var examAvg = 0;
+  for (var i = 0; i < exams.length; i++) {
+    examAvg = (examAvg + parseInt(exams[i].score)) / (i+1);
+  }
+  $scope.examAverage = examAvg + "%";
 
   //Function to display tab changes in scope
   $scope.changeTab = function(filter) {
