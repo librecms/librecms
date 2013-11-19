9aac5419469c7e2daf0c6b63de2bd71c8be46e3d
mbe5038 <espejo.marius@gmail.com>
2013-11-19 14:39:43 -0500

    changed calendar controller to recognize assignments as events

--- src/angular/app/scripts/controllers/calendar.js
+++ src/angular/app/scripts/controllers/calendar.js
@@ -4,7 +4,7 @@ angular.module('librecmsApp')
 .controller('CalendarCtrl', function($scope, Restangular) {
 
   function getUserEvents() {
-    console.log('getUserEvents = ' + JSON.stringify($scope.user));
+    //console.log('getUserEvents = ' + JSON.stringify($scope.user));
     var startOfThisMonth = moment(new Date()).startOf('month').toDate().getTime();
     Restangular.one('users', $scope.user._id).getList('events', {start: startOfThisMonth}).then(function(events) {
       // Gather events from API and reformat their
@@ -18,11 +18,31 @@ angular.module('librecmsApp')
     });
   }
 
+  function getCourseAssignments() {
+    //console.log('getCourseAssignments = ' + JSON.stringify($scope.user));
+    var startOfThisMonth = moment(new Date()).startOf('month').toDate().getTime();
+    Restangular.one('courses', $scope.course._id).getList('assignments', {start: startOfThisMonth}).then(function(assignments) {
+      // Gather assignments from API and reformat their
+      // start and end components into Javascript Date objects
+      $scope.assignments = assignments;
+      $scope.assignments.map(function(assignment) {
+        assignment.start = new Date(assignment.posted);
+        assignment.end = new Date(assignment.due);
+        assignment.color = '#e74c3c' //move this somewhere else 
+      });
+      $scope.eventSources.push($scope.assignments);
+    });
+  }
+
   $scope.events = [];
   $scope.eventSources = $scope.events;
 
   if ($scope.user && $scope.user._id) {
     getUserEvents();
+  }
+
+  if ($scope.course){
+    getCourseAssignments();
   }
 
   $scope.$on('UserService.update', getUserEvents);
