d89f01da7d101c992213a89e58b34bd15cb7a6ab
Nick <nbuhay@gmail.com>
2013-12-04 17:59:29 -0500

    Start date now checks current Date instead of beginning of the month

--- src/angular/app/scripts/controllers/eventbar.js
+++ src/angular/app/scripts/controllers/eventbar.js
@@ -6,27 +6,22 @@ angular.module('librecmsApp')
   function getUserEvents() {
     var user = UserService.getUser();
     if(user) {
-      var startOfThisMonth = moment(new Date()).startOf('month').toDate().getTime();
+      var startOfThisMonth = new Date().getTime();
       Restangular.one('users', user._id).getList('events', {start: startOfThisMonth}).then(function(events) {
         // Gather events from API and reformat their
         // start and end components into Javascript Date objects
+        console.log(JSON.stringify(events));
         $scope.events = events;
         $scope.events.map(function(event) {
-          event.start = new Date(event.start);
-          event.end = new Date(event.end);
+          event.start = new Date(event.due);
+          event.end = new Date(event.due);
         });
       });
     } 
   }
 
   $scope.toggleEventStatus = function(param) {
-    var Event = Restangular.one('users', $scope.user._id).one('events', param).get()
-        .then(function(event) {
-        console.log(JSON.stringify(event));
-        if(event.completed == false) {
-          console.log("YES");
-        }
-        });
+    var Event = Restangular.one('users', $scope.user._id).one('events', param).post();
   }
 
   if ($scope.user || $scope.user === undefined) {
@@ -34,24 +29,21 @@ angular.module('librecmsApp')
   }
 
   $scope.$on('UserService.update', getUserEvents);
-
-  /* event sources array */
-  $scope.test = [$scope.events];
-
+  
   $scope.predicate = 'start';
   $scope.quantity = 5;
 
 });
 
-angular.module('librecmsApp').filter('upcoming', function() {
-  return function(e) {
-    var date = new Date();
-    var returnArray = new Array();
-    for(var i = 0; i < e.length; i++) {
-      if(date.toISOString() <= e[i].start.toISOString()) {
-        returnArray.push(e[i]);
-      }
-    }
-    return returnArray;
-  }
-});
+// angular.module('librecmsApp').filter('upcoming', function() {
+//   return function(a) {
+//     console.log(typeof(a));
+//     var returnArray = new Array();
+//     for(var i = 0; i < a.length; i++) {
+//       if(Date.now() - a[i].start.getTime() < 0) {
+//         returnArray.push(a[i]);
+//       }
+//     }
+//     return returnArray;
+//   }
+// });
